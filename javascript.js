343e3149b5655e2197071fe6cf30bdf2378ffa1a
Moisés santos <158361678+MoisesS01@users.noreply.github.com>
2024-02-08 00:45:28 -0300

    altura e som ok

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
 const lampada = document.getElementById('lampada');
+let quebrada = false;
 
 lampada.addEventListener('mouseover', function() {
-    this.src = '1lampadaA.png';
+    if (!quebrada) {
+        this.src = '1lampadaA.png';
+    }
 });
 
 lampada.addEventListener('click', function() {
-    this.src = '3lampadaC.png';
-    const audio = new Audio('quebra.mp3');
-    audio.play();
+    if (!quebrada) {
+        this.src = '3lampadaC.png';
+        const audio = new Audio('quebra.mp3');
+        audio.play();
+        quebrada = true;
+    }
 });
